af870bce45210ea825f98cf19dd259e0142bc46e
yell <rainbow391492006@vip.qq.com>
2016-07-27 19:37:56 +0800

    使用了browserify打包js，可以通过sourceMap看没有压缩之前的代码

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,11 @@ var uglify = require('gulp-uglify');
 var imagemin = require('gulp-imagemin');
 var htmlmin = require('gulp-htmlmin');
 var plumber = require('gulp-plumber');
+var browserify = require('browserify');
+var streamify = require('gulp-streamify')
+var source = require('vinyl-source-stream');
+var sourcemaps = require("gulp-sourcemaps");
+var buffer = require('vinyl-buffer');
 var reload = browserSync.reload;
 
 
@@ -64,8 +69,22 @@ gulp.task('html', function() {
 });
 
 
+//打包JS
+gulp.task('bundle', function() {  
+  return browserify({
+    entries: "./src/js/main.js",
+    debug: true //debug: true是告知Browserify在运行同时 生成 内联sourcemap 用于调试。
+  })
+    .bundle() 
+    .pipe(source('bundle.min.js'))//vinyl-source-stream用于将Browserify的bundle()的输出转换为Gulp可用的[vinyl][]（一种虚拟文件格式）流。
+    .pipe(buffer())//vinyl-buffer用于将vinyl流转化为buffered vinyl文件（gulp-sourcemaps及大部分Gulp插件都需要这种格式）。
+    .pipe(sourcemaps.init({loadMaps: true}))//引入gulp-sourcemaps并设置loadMaps: true是为了读取上一步得到的内联sourcemap，并将其转写为一个单独的sourcemap文件。
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest('dist/js'));
+});
+
 //启动服务
-gulp.task('serve', ['less','script','imagemin','html'],function() {
+gulp.task('serve', ['less','bundle','imagemin','html'],function() {
     browserSync.init({
         server: {
             baseDir: "./"
@@ -73,7 +92,7 @@ gulp.task('serve', ['less','script','imagemin','html'],function() {
     });
 
     gulp.watch('src/img/*',['imagemin']).on('change',reload)
-    gulp.watch('src/js/*.js',['script']).on('change',reload)
+    gulp.watch('src/js/*.js',['bundle']).on('change',reload)
     gulp.watch('src/style/*.less',['less']).on('change',reload)
     gulp.watch('src/*.html',['html']).on('change',browserSync.reload)
 
